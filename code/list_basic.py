bfb166996b3070a22282fc761a09b99528037567
Akash N <akashnatraj366@gmail.com>
2026-04-21 12:32:46 +0530

    code: loops and list basics

---
+++ code/list_basic.py
@@ -0,0 +1,12 @@
+a = [1,2,3]
+
+a.append(4)
+print(a)
+
+a.remove(2)
+print(a)
+
+
+
+.....................
+CBR&P
---
+++ code/loops_test.py
@@ -0,0 +1,12 @@
+nums = [1,2,3,4]
+
+for i in nums:
+    print(i)
+
+for i in range(5):
+    print(i)
+
+# tried nested also
+for i in range(2):
+    for j in range(2):
+        print(i, j)
