09fe6ee18516a1a889856cd782a93dd3a2fec309
Akash <72803655+Akash642-eng@users.noreply.github.com>
2026-04-22 19:36:33 +0530

    Optimize isAnagram function using Counter
    Refactored isAnagram function to use Counter for improved performance.

--- leetcode/valid_anagram.py
+++ leetcode/valid_anagram.py
@@ -2,3 +2,16 @@ def isAnagram(s, t):
     return sorted(s) == sorted(t)
 
 print(isAnagram("anagram", "nagaram"))
+
+..........
+O(n log n)
+.............................
+
+from collection import Counter
+
+def isAnagram(s, t):
+    return Counter(s) == Counter(t)
+
+print(isAnagram("anagram", "nagaram"))
+..........
+o(n) - bit fast for big strings...
