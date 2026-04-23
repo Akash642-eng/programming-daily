d9b5289af416001ee33d08b7a051dcde10b7b56f
Akash N <akashnatraj366@gmail.com>
2026-04-23 12:31:55 +0530

    Zeros

---
+++ leetcode/move_zeroes.py
@@ -0,0 +1,14 @@
+def moveZeros(nums):
+    j = 0
+
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[j], nums[i] = nums[i], nums[j]
+            j += 1
+
+    return nums
+
+print(moveZeros([0,1,0,3,12]))
+
+
+
